700061ffd92eb3d672615ca08334d7a4086fd53d
margokovacs82 <margo.kovacs@gmail.com>
2018-06-27 20:44:38 +0200

    dev environment on

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+render(
+  <AppContainer>
+    <h1>Hello world!</h1>
+    </AppContainer>,
+    document.getElementById('app'),
+);
